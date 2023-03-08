cbb250ae09e9ca3edb7019ee55853a454b1021e3
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-08 10:47:53 -0300

    added few strings

---
+++ tipos/04-metodos-string.py
@@ -0,0 +1,13 @@
+animal = "   Michi loco   "
+print(animal.upper())
+print(animal.lower())
+print(animal.strip().capitalize())
+print(animal.title())
+print(animal.strip())
+print(animal.lstrip())
+print(animal.rstrip())
+print(animal.find("ch"))
+print(animal.replace("chi","cho"))
+print("chi" in animal)
+print("chi" not in animal)
+
