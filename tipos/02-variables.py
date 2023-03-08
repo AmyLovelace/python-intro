1e80c825ec4eed1d3f6bf3e5cc96f831c0281c6f
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-08 00:02:17 -0300

    added a python file

---
+++ tipos/02-variables.py
@@ -0,0 +1,11 @@
+nombre_curso = "Ultimate Python"
+descripcion_curso = """
+ultimate python , este curso contempla todos los conocimientos que necesitas aprender para conseguir un trabajo como programador"""
+
+
+print(len(nombre_curso))
+print(nombre_curso[0])
+print(nombre_curso[0:8])
+print(nombre_curso[9:])
+print(nombre_curso[:8])
+print(nombre_curso[:])
