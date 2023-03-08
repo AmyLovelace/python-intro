223e361e9130dd60a3a8b3b6b11b096ccee9e55a
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-07 23:42:21 -0300

    added a python file

---
+++ tipos/01-variables.py
@@ -0,0 +1,8 @@
+nombre_curso = "Ultimate Python"
+nombre1 = "Diego"
+NOMBRE_CURSO = "pancito"
+NoMbRe_CuRsO = "katiuska"
+NombreCurso = "chechi"
+
+
+print(nombre_curso, NoMbRe_CuRsO, nombre1, NOMBRE_CURSO, NombreCurso)
