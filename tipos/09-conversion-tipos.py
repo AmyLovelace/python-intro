29a79c58ea6b0c7b2fdd8c26110b57125725fe2d
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-09 23:01:06 -0300

    x-input exercise

---
+++ tipos/09-conversion-tipos.py
@@ -0,0 +1,13 @@
+#x = input ("")#
+
+#int()
+#str() agarra cualquier dato y lo vuelve una string 
+#float()agarra los datos y los convierte en float
+#bool()The object will always return True, unless:The object is empty, like [], (), {} The object is False The object is 0
+
+print(bool("")) #falso
+print(bool("0")) # true es un string con el valor de 0
+print(bool("none"))# falso
+print(bool(""))#true
+print(bool(0))#falso
+
