bc9cdd23a2c780f23c36574077c2f8128226bff3
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-09 21:15:16 -0300

    added math function

---
+++ tipos/07-numeros-funciones.py
@@ -0,0 +1,13 @@
+import math  #codigo escrito en pitón
+
+print(round(1.3))
+print(round(1.7))
+print(round(1.5))
+print(abs(-77))  #valorabsoluto
+print(abs(55))
+
+print(math.ceil(1.1)) #lleva el resultado al numero entero  mas alto 
+print(math.floor(1.9999)) #lleva el valor al numero entero  mas bajo
+print(math.isnan(23))#"NO ES UN NUMERO" EN VERDADERO O FALSO
+print(math.pow(10, 3))#elevar un numero a la potencia del 2do numero 
+print(math.sqrt(9))#expresa la raiz cuadrada 
